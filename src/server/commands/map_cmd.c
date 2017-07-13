8d47629fb516103b63f78af319e4ec3a2d763a07
Guillaume MARDON <contact@gmardon.com>
2017-07-13 16:04:27 +0200

    fix gamemap

--- src/server/commands/map_cmd.c
+++ src/server/commands/map_cmd.c
@@ -8,16 +8,18 @@ int handle_map(char *cmd, t_client *client, t_server *server)
     int x;
 
     y = 0;
-    x = 0;
     index = 0;
     cells = my_malloc((server->gamemap->height * server->gamemap->width) * sizeof(char));
     while (y < server->gamemap->height)
     {
+        x = 0;
         while (x < server->gamemap->width)
         {
             cells[index] = server->gamemap->cells[y][x];
             index++;
+            x++;
         }
+        y++;
     }
-    send_message(client, "MAP %i %i %s", server->gamemap->width, server->gamemap->height, cells);
+    send_message(client, "MAP %i %i %s\n", server->gamemap->width, server->gamemap->height, cells);
 }
