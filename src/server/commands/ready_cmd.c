a87042a111d276c6e6f7e2657ef3ba2af7bcda4f
Guillaume MARDON <contact@gmardon.com>
2017-07-17 16:41:50 +0200

    fix ready bug

--- src/server/commands/ready_cmd.c
+++ src/server/commands/ready_cmd.c
@@ -3,21 +3,25 @@
 int handle_ready(char *cmd, t_client *client, t_server *server)
 {
     t_clist	*tmp;
+    int ready_count;
     int not_ready;
 
     client->state = CLIENT_STATE_READY;
     not_ready = 0;
+    ready_count = 0;
     tmp = server->client_list;
-    while (tmp != NULL)
+    while (tmp != NULL && tmp->client != NULL)
     {
         if (tmp->client->state == CLIENT_STATE_CONNECTED)
             not_ready = 1;
+        else
+            ready_count++;
         tmp = tmp->next;
     }
-    if (!not_ready)
+    if (!not_ready && ready_count == 2)
     {
         tmp = server->client_list;
-        while (tmp != NULL)
+        while (tmp != NULL && tmp->client != NULL)
         {  
             send_message(tmp->client, "START\n");
             tmp = tmp->next;
